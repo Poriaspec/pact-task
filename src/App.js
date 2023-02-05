9e9d46b481508b317cd8b09e2731fb31f39b5626
PEACE IRABOR <poriaspec@gmail.com>
2023-02-05 03:56:52 +0100

    new update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import "./index.css"
 import Homepage from './pages/Homepage';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ScrollButton from "./components/ScrollButton/ScrollButton"
 
 
 function App() {
+
+  function ScrollToTopOnMount() {
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
+    return null;
+  }
+  
   
   return (
 
     <div>
+        <ScrollToTopOnMount />
       <Router>
         <Routes>
           <Route path='/' element={<Homepage/>} />
         </Routes>
       </Router>
+      <ScrollButton/>
     </div>
 
   
